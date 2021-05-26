e7e35bb2b988273091d51efb7c12a777e219fe31
mildtotoro <pimrampha.m@gmail.com>
2021-05-26 11:56:14 +0700

    integrate add book page

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { CrudService } from './../../service/crud.service';
+import { FormGroup, FormBuilder, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.scss']
+  styleUrls: ['./add-book.component.scss'],
 })
 export class AddBookComponent implements OnInit {
+  bookForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private crudService: CrudService
+  ) {
+    this.bookForm = this.formBuilder.group({
+      name: [''],
+      price: [''],
+      description: [''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(): any {
+    this.crudService.AddBook(this.bookForm.value).subscribe(
+      () => {
+        console.log('Data added successfully');
+        this.ngZone.run(() => {
+          this.router.navigateByUrl('/book-list');
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
---
+++ src/app/service/crud.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+
+export class Book {
+  _id!: String;
+  name!: String;
+  price!: String;
+  description!: String;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CrudService {
+  // Node/Express API
+  REST_API = 'http://localhost:8000/api';
+
+  // Http header
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application-json');
+  constructor(private httpClient: HttpClient) {}
+
+  AddBook(data: Book): Observable<any> {
+    let API_URL = `${this.REST_API}/add-book`;
+    return this.httpClient
+      .post(API_URL, data)
+      .pipe(catchError(this.handleError));
+  }
+
+  // get all objects
+  GetBooks() {
+    return this.httpClient.get(`${this.REST_API}`);
+  }
+
+  // get single object
+  GetBook(id: any): Observable<any> {
+    let API_URL = `${this.REST_API}/read-book/${id}`;
+    return this.httpClient.get(API_URL, { headers: this.httpHeaders }).pipe(
+      map((res: any) => {
+        return res || {};
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  // update
+  UpdateBook(id: any, data: any): Observable<any> {
+    let API_URL = `${this.REST_API}/update-book/${id}`;
+    return this.httpClient
+      .put(API_URL, data, { headers: this.httpHeaders })
+      .pipe(catchError(this.handleError));
+  }
+
+  // delete
+  DeleteBook(id: any): Observable<any> {
+    let API_URL = `${this.REST_API}/delete-book/${id}`;
+    return this.httpClient
+      .delete(API_URL, { headers: this.httpHeaders })
+      .pipe(catchError(this.handleError));
+  }
+
+  // error
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    console.log({ error });
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error code: ${error.error.code} \n message ${error.error.message}`;
+    }
+    console.log({ errorMessage });
+    return throwError(errorMessage);
+  }
+}
